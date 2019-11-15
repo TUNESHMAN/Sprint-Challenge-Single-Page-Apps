dfb0f595671f0f5afe375974842418ea73ab0145
Babatunde <babatundea15@gmail.com>
2019-11-15 12:04:08 +0100

    I added buttons and set state

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,44 @@
 import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+import styled from "styled-components";
+
+const Buttons = styled.button`
+width: 200px;
+height: 40px;
+background-color: teal;
+border-radius: 10px;
+outline: none;`
+
+const Outer = styled.div`
+margin-bottom: 100px;`
 
 export default function CharacterCard(props) {
+  const [picture, setPicture] = React.useState(true);
   return (
-    <div>
-      <span>{props.name}</span>;<p>{props.gender}</p>
-  <p>{props.status}</p>
-<h3>{props.id}</h3>
-<img src = {props.image} />
-  <h3>{props.species}</h3>
-    </div>
+    <Outer>
+      <Card>
+        <CardBody>
+          <CardTitle>{props.name}</CardTitle>
+          <CardText>
+          <h3>ID: {props.id}</h3>
+          <p>Gender: {props.gender}</p>
+          <p>Status: {props.status}</p>
+          {picture ? <img src={props.image} /> : null}
+          <h3>{props.species}</h3>
+          </CardText>
+          <Buttons onClick={() => setPicture(!picture)}>
+          {picture ? "HIDE IMAGE" : "SHOW IMAGE"}
+        </Buttons>
+        </CardBody>
+      </Card>
+    </Outer>
   );
 }
